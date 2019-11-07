ff9b48492a33220f6e4f8e550c63f897a100da54
Wendell Liu <cuk.bas@gmail.com>
2019-11-07 22:02:33 +0800

    logger

--- cmd/main.go
+++ cmd/main.go
@@ -1,35 +1,31 @@
 package main
 
 import (
-	"os"
-
 	"github.com/wendellliu/good-search/pkg/config"
+	"github.com/wendellliu/good-search/pkg/logger"
 	"github.com/wendellliu/good-search/pkg/mongo"
 	"github.com/wendellliu/good-search/pkg/mongo/dto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/joho/godotenv"
-	log "github.com/sirupsen/logrus"
+	"github.com/sirupsen/logrus"
 )
-
-func init() {
-	//log.SetFormatter(&log.JSONFormatter{})
-	log.SetOutput(os.Stdout)
-}
 
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Fatal("Error loading .env file")
+		logger.Logger.Fatal("Error loading .env file")
 	}
 
 	config.Load()
 	mongo.Load()
-	log.Info("start")
+	logger.Load()
+
+	logger.Logger.Info("start")
 
 	name := "富台機械開發建設有限公司"
 	company := dto.GetCompany(mongo.DB, &dto.CompanyParams{Name: &name})
-	log.WithFields(log.Fields{"company": company}).Info("get result")
+	logger.Logger.WithFields(logrus.Fields{"company": company}).Info("get result")
 
 	var capital int = 500000
 	cursorID, _ := primitive.ObjectIDFromHex("577f9f9cd4c98d1f8749efbd")
@@ -41,10 +37,10 @@ func main() {
 	companies := dto.GetCompanies(mongo.DB, &dto.CompanyParams{Capital: &capital}, options)
 	for i, company := range companies {
 
-		log.WithFields(
-			log.Fields{"index": i},
+		logger.Logger.WithFields(
+			logrus.Fields{"index": i},
 		).WithFields(
-			log.Fields{"companies": company},
+			logrus.Fields{"companies": company},
 		).Info("get result")
 
 	}
