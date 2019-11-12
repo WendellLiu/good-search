fe5889ae7f660e4d9e0e4ee869b0751ece0c83ef
Wendell Liu <cuk.bas@gmail.com>
2019-11-12 20:46:22 +0800

    seperate dto

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,9 @@ package main
 
 import (
 	"github.com/wendellliu/good-search/pkg/config"
+	"github.com/wendellliu/good-search/pkg/dto"
 	"github.com/wendellliu/good-search/pkg/logger"
 	"github.com/wendellliu/good-search/pkg/mongo"
-	"github.com/wendellliu/good-search/pkg/mongo/dto"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 
 	"github.com/joho/godotenv"
@@ -44,21 +44,21 @@ func main() {
 		).Info("get result")
 
 	}
-	var capital int = 500000
-	cursorID, _ := primitive.ObjectIDFromHex("577f9f9cd4c98d1f8749efbd")
-	options := dto.Options{
-		Limit:    5,
-		CursorID: cursorID,
-	}
+	//var capital int = 500000
+	//cursorID, _ := primitive.ObjectIDFromHex("577f9f9cd4c98d1f8749efbd")
+	//options := dto.Options{
+	//Limit:    5,
+	//CursorID: cursorID,
+	//}
 
-	companies := dto.GetCompanies(mongo.DB, &dto.CompanyParams{Capital: &capital}, options)
-	for i, company := range companies {
+	//companies := dto.GetCompanies(mongo.DB, &dto.CompanyParams{Capital: &capital}, options)
+	//for i, company := range companies {
 
-		logger.Logger.WithFields(
-			logrus.Fields{"index": i},
-		).WithFields(
-			logrus.Fields{"companies": company},
-		).Info("get result")
+	//logger.Logger.WithFields(
+	//logrus.Fields{"index": i},
+	//).WithFields(
+	//logrus.Fields{"companies": company},
+	//).Info("get result")
 
-	}
+	//}
 }
