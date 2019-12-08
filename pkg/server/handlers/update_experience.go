8ec16a9460d6fac91b4918e3eaad26e829a0f4cf
Wendell Liu <cuk.bas@gmail.com>
2019-12-08 20:26:45 +0800

    get experience by id

--- pkg/server/handlers/update_experience.go
+++ pkg/server/handlers/update_experience.go
@@ -2,14 +2,21 @@ package handlers
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/sirupsen/logrus"
+	"github.com/wendellliu/good-search/pkg/dto"
+	"github.com/wendellliu/good-search/pkg/logger"
 	pb "github.com/wendellliu/good-search/pkg/pb"
 )
 
 func (s *Server) UpdateExperience(ctx context.Context, req *pb.UpdateExperienceReq) (*pb.UpdateExperienceResp, error) {
+	experience, err := dto.GetExperience(context.Background(), s.DB, req.Id)
 
-	//if err != nil {
-	//logger.Logger.Error(err)
-	//}
+	if err != nil {
+		logger.Logger.Error(err)
+	}
+	logger.Logger.WithFields(logrus.Fields{"experience": fmt.Sprintf("%+v", experience)}).Info("get result")
+
 	return &pb.UpdateExperienceResp{Status: pb.Status_SUCCESS}, nil
 }
