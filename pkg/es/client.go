fdfc62a45ef05909639e4685bdc517ed143f02e2
Wendell Liu <cuk.bas@gmail.com>
2020-01-22 23:05:49 +0800

    TODO: map ids from experiences search response

--- pkg/es/client.go
+++ pkg/es/client.go
@@ -54,11 +54,46 @@ func (es *Elasticsearch) IndexExperience(ctx context.Context, experience dto.Exp
 		Refresh:    "true",
 	}
 
-	res, err := req.Do(context.Background(), es.Client)
+	res, err := req.Do(ctx, es.Client)
 	if err != nil {
 		return err
 	}
 	defer res.Body.Close()
 
 	return err
+}
+
+func (es *Elasticsearch) SearchExperiences(ctx context.Context, keyword string) (
+	experienceIds []string,
+	err error,
+) {
+	localLogger := logger.Logger.WithFields(
+		logrus.Fields{"endpoint": "es-SearchExperiences"},
+	)
+	client := es.Client
+
+	var buf bytes.Buffer
+	query := map[string]interface{}{
+		"query": map[string]interface{}{
+			"multi_match": map[string]interface{}{
+				"query":  keyword,
+				"fields": config.Config.Search.ExperiencesSearch.Fields,
+			},
+		},
+		"_source": []string{"title"},
+	}
+	if err := json.NewEncoder(&buf).Encode(query); err != nil {
+		localLogger.Fatalf("Error encoding query: %s", err)
+	}
+
+	req := esapi.SearchRequest{
+		Index:          []string{EXPERIENCE_INDEX},
+		Body:           &buf,
+		TrackTotalHits: true,
+	}
+
+	res, err := req.Do(ctx, client)
+
+	localLogger.Infof("res: %+v \n", res)
+	return []string{"123", "321"}, nil
 }
--- pkg/server/handlers/search_experiences.go
+++ pkg/server/handlers/search_experiences.go
@@ -7,6 +7,7 @@ import (
 )
 
 func (s *Server) SearchExperiences(ctx context.Context, req *pb.SearchExperiencesReq) (*pb.SearchExperiencesResp, error) {
+	s.Es.SearchExperiences(ctx, req.Keyword)
 	return &pb.SearchExperiencesResp{
 		Status: pb.Status_SUCCESS,
 		ExperienceIds: []string{
